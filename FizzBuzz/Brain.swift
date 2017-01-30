65d672a28050189deb83e8a65f163eb4effea38e
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-30 14:43:04 +0000

    App says Fizz and buzz, also refactored methods in Brain class

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -10,26 +10,26 @@ import Foundation
 
 class Brain: NSObject {
     func isDivisibleByThree(number: Int) -> Bool {
-        if number % 3 == 0 {
-            return true
-        } else {
-            return false
-        }
+        return isDivisibleBy(divisor: 3, number: number)
     }
     
     func isDivisibleByFive(number: Int) -> Bool {
-        if number % 5 == 0 {
-            return true
-        } else {
-            return false
-        }
+        return isDivisibleBy(divisor: 5, number: number)
     }
     
     func isDivisibleByFifteen(number: Int) -> Bool {
-        if number % 15 == 0 {
-            return true
+        return isDivisibleBy(divisor: 15, number: number)
+    }
+    
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+        }
+    
+    func check(number: Int) -> String {
+        if isDivisibleByThree(number: number) {
+            return "Fizz"
         } else {
-            return false
+            return "Buzz"
         }
     }
     
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -52,4 +52,14 @@ class BrainTests: XCTestCase {
         XCTAssertEqual(result, false)
     }
     
+    func testSayFizz() {
+        let result = brain.check(number: 3)
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func testSayBuzz() {
+        let result = brain.check(number: 5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
 }
