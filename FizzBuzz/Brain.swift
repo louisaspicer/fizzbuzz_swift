4f3b40993b81b4b04455caf63f60cf07e2df1fcd
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-30 12:49:34 +0000

    First test passed

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,16 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Pea M. Tyczynska on 30/01/2017.
+//  Copyright © 2017 Makers Academy. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+    func isDivisibleByThree(number: Int) -> Bool {
+        return true
+    }
+    
+}
---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,30 @@
+//
+//  BrainTests.swift
+//  FizzBuzz
+//
+//  Created by Pea M. Tyczynska on 30/01/2017.
+//  Copyright © 2017 Makers Academy. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
+    
+}
